f27d4bb9ef37769a5ebc3e9d9c304e33847bc0ec
asmaabahr <asmaa559632@gmail.com>
2023-03-17 18:04:55 -0700

    my commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Determine if the last digit of a random number is
@@ -11,20 +11,20 @@
 
 int main(void)
 {
-	int n;
-	char l[] = "Last digit of";
+	int n, digit;
 
+	digit = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	printf("%s %d is %d and is ", l, n, n % 10);
 
-	if (n % 10 > 5)
+	if (digit > 5)
 		printf("greater than 5\n");
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (digit < 6 && digit != 0)
 		printf(" less than 6 and not 0\n");
-	else
+	else if (digit == 0)
 		printf("0\n");
 
 	return (0);
