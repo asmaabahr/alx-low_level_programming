2c90677ac4d0481c009013c9d332ce3e23dbab80
asmaabahr <asmaa559632@gmail.com>
2023-03-17 17:47:00 -0700

    my commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,9 +22,10 @@ int main(void)
 
 	if (n % 10 > 5)
 		printf("greater than 5\n");
-	else if (n % 10 == 0)
-		printf("0\n");
-	else
+	else if ((n % 10) < 6 && (n % 10) != 0)
 		printf(" less than 6 and not 0\n");
+	else
+		printf("0\n");
+
 	return (0);
 }
