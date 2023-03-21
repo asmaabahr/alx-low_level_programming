e3d0e3f870913c5c89f2e2c701128a383b1beb4a
asmaabahr <asmaa559632@gmail.com>
2023-03-21 13:41:24 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,30 +5,31 @@
 */
 void times_table(void)
 {
-	int num, mult, prod;
+	int i, j, res;
 
-	for (num = 0; num <= 9; num++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (j = 0; j <= 9; j++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-
-			prod = num * mult;
-
-			if (prod <= 9)
+			res = (i * j);
+			if (j != 0)
 			{
+				_putchar(',');
 				_putchar(' ');
 			}
-			else
+			if (res >= 10)
 			{
-				_putchar((prod / 10) + '0');
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
 			}
-			_putchar((prod % 10) + '0');
+			else if (res < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
 		}
-												_putchar('\n');
-	}
+		_putchar('\n');
+											}
 }
